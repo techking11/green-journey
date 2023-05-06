abbc79dc23be8fb2627d72510efe33b145e9ecb2
techking11 <techking1132000@gmail.com>
2023-05-06 14:55:49 +0630

    first commit

---
+++ js/script/script.js
@@ -0,0 +1,80 @@
+/* 
+Main Navigation
+---------------------------------------------------------------- */
+(function ($) {
+    "use strict";
+
+    $(function () {
+        var header = $(".start-style");
+        $(window).scroll(function () {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 10) {
+                header.removeClass('start-style').addClass("scroll-on");
+            } else {
+                header.removeClass("scroll-on").addClass('start-style');
+            }
+        });
+    });
+
+
+    //Menu On Hover
+    $('body').on('mouseenter mouseleave', '.nav-item', function (e) {
+        if ($(window).width() > 750) {
+            var _d = $(e.target).closest('.nav-item');
+            _d.addClass('show');
+            setTimeout(function () {
+                _d[_d.is(':hover') ? 'addClass' : 'removeClass']('show');
+            }, 1);
+        }
+    });
+
+    //Switch light/dark
+
+    $("#switch").on('click', function () {
+        if ($("body").hasClass("dark")) {
+            $("body").removeClass("dark");
+            $("#switch").removeClass("switched");
+        } else {
+            $("body").addClass("dark");
+            $("#switch").addClass("switched");
+        }
+    });
+
+})(jQuery);
+
+
+/* Main Navigation --- */
+
+
+/* 
+Footer 
+------------------------------------------------------------------------------ */
+// Back to top button
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
+
+
+
+var position = $(window).scrollTop();
+
+// should start at 0
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > position) {
+        console.log('scrollDown');
+        $('.navigation-wrap').fadeOut("slow");
+
+    } else {
+        console.log('scrollUp');
+        $('.navigation-wrap').fadeIn("slow");
+
+    }
+    position = scroll;
+});
